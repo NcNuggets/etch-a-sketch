7063c56dbaa8539b126cb88f870c839bfa6c7c2e
Nick <pucillo.nick@gmail.com>
2018-02-24 23:20:37 -0500

    initial file setup for etch-a-sketch project

---
+++ scripts/etch-a-sketch.js
@@ -0,0 +1,33 @@
+/*  TO DO / HELPFUL PROPERTIES
+    -- need to create a grid with js to satisify the project.
+    -- need to create variable grid setup so that a user can enter any number between x and y to create a grid with those square dimensions, but using the same pixel area (960x960 say).
+    -- the new grid should generate in the same total space as before (e.g. 960px wide) to create a new sketch pad
+    -- can target the #container div inside index.html.
+    -- set BORDERS and MARGINS to 0?
+    -- need to set a div:hover action to change the content/background-color.
+    -- need to use event listeners to look for the hover action start point in order to trigger the change.
+    -- change the color of the divs by changing the css class.
+    -- need to add a button that resets the grid.
+    -- push to github
+
+    ELEMENTS:
+    <div>
+    <button>
+
+    PROPERTIES:
+    grid-template-columns
+    grid-template-rows
+    grid-auto-columns
+    grid-auto-rows
+    grid-auto-flow
+    grid-row-gap
+    grid-column-gap
+    x:hover
+    background-color
+
+    FUNCTIONS:
+    repeat()
+
+    DATA TYPES:
+    <flex> -- 1fr, 2fr, etc.
+*/
