5f73bbaa55bbe0611179266c34092e9a351671fe
Nick <pucillo.nick@gmail.com>
2018-02-26 23:34:05 -0500

    added buttons to adjust the size of the grid along with some functions to set the grid size

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -37,18 +37,87 @@
 
 // can target the #container div inside index.html to append all the child nodes
 const container = document.querySelector('.container');
-let gridWidth = 80;
-let gridHeight = 80;
+
+// create 3 buttons to set grid size
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('container-buttons');
+
+let gridButton10 = document.createElement('button');
+gridButton10.classList.add('buttons');
+gridButton10.textContent = '10 x 10';
+gridButton10.addEventListener('click', () => {
+  setGrid(10, 10);
+});
+
+let gridButton25 = document.createElement('button');
+gridButton25.classList.add('buttons');
+gridButton25.textContent = '25 x 25';
+gridButton25.addEventListener('click', () => {
+  setGrid(25, 25);
+});
+
+let gridButton50 = document.createElement('button');
+gridButton50.classList.add('buttons');
+gridButton50.textContent = '50 x 50';
+gridButton50.addEventListener('click', () => {
+  setGrid(50, 50);
+});
+
+buttonContainer.appendChild(gridButton10);
+buttonContainer.appendChild(gridButton25);
+buttonContainer.appendChild(gridButton50);
+
+const body = document.querySelector('body');
+body.appendChild(buttonContainer);
+
+// make a reset button
+let resetButton = document.createElement('button');
+resetButton.classList.add('buttons');
+resetButton.classList.add('button-reset');
+resetButton.textContent = 'Reset';
+resetButton.addEventListener('click', resetGrid);
+
+buttonContainer.appendChild(resetButton);
+
+// clear the grid
+function clearGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function resetGrid() {
+  clearGrid();
+  setGrid(10, 10);
+}
 
 // need to create a div for each grid block using js
-for (i = 0; i < (gridWidth * gridHeight); i++) {
-  let div = document.createElement('div');
-  div.classList.add('block-clear');
-  div.addEventListener('mouseover', (e) => {
-    div.classList.remove('block-clear');
-    div.classList.add('block-filled');
-  });
-  container.appendChild(div);
+function setGrid(width, height) {
+  clearGrid();
+
+  for (i = 0; i < (width * height); i++) {
+    let block = document.createElement('div');
+    block.classList.add('block');
+    block.addEventListener('mouseover', (e) => {
+      block.classList.add('block-filled');
+    });
+    container.appendChild(block);
+    // let divList = container.childNodes;
+  }
+
+
+/*
+  for (i = 0; i < divList.length; i++) {
+    // need to change the size of the blocks
+  }
 }
+*/
+
+/*.block {
+  width: 5px;
+  height: 5px;
+  float: left;
+  background-color: white;
+}*/
 
 // need to add a hover listener to change the class of my block divs from clear to filled
